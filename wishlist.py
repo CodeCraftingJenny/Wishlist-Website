36396db7a8c727db945673a4415f028fa768348a
CodeCraftingJenny <157246398+CodeCraftingJenny@users.noreply.github.com>
2024-03-23 20:34:22 -0700

    fixed remove button

--- wishlist.py
+++ wishlist.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 items = []
@@ -8,21 +8,22 @@ def list_append(item):
     items.append(item)
     
 def list_delete(item):
-    global wishlist
-    wishlist.remove(item)
+    global items
+    items.remove(item)
     
-@app.route("/", method = ['GET','POST'])
-def wishlist():
+@app.route("/", methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
         item_added = request.form['wishlist_item']
         if item_added != '':
             list_append(item_added)
             
-    return render_template('wish.html',wishlist = wishlist)
+    return render_template('wish.html', wishlist=items)
 
-@app.route('/delete/<item>')
+@app.route('/remove/<item>')
 def delete(item):
     list_delete(item)
-    return redirect(url_for('wishlist'))
+    return redirect(url_for('index'))
 
-# app.run(debug = True) 
+if __name__ == '__main__':
+    app.run(debug=True)
