b5411cf4a9bef07ce2c13ded391f476fcb3c2e21
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-03-23 19:58:12 -0700

    added delete funtion

--- wishlist.py
+++ wishlist.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,url_for
 
 app = Flask(__name__)
 items = []
@@ -6,11 +6,23 @@ items = []
 def list_append(item):
     global items
     items.append(item)
-@app.route("/", method = ['GET','POST'])
-def hello_world():
-    if request.method == 'POST':
-    item_added = request.form['wishlist_item']
     
-    return render_template('wish.html')
+def list_delete(item):
+    global wishlist
+    wishlist.remove(item)
+    
+@app.route("/", method = ['GET','POST'])
+def wishlist():
+    if request.method == 'POST':
+        item_added = request.form['wishlist_item']
+        if item_added != '':
+            list_append(item_added)
+            
+    return render_template('wish.html',wishlist = wishlist)
 
-app.run(debug = True) 
+@app.route('/delete/<item>')
+def delete(item):
+    list_delete(item)
+    return redirect(url_for('wishlist'))
+
+# app.run(debug = True) 
